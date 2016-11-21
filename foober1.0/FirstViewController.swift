d91cdb9ca435e9f68431ba5a4a152185fe989461
Nhan Tran <nhantrantnt@gmail.com>
2016-11-20 20:00:44 -0700

    setup multi tabs and map view

--- foober1.0/FirstViewController.swift
+++ foober1.0/FirstViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
